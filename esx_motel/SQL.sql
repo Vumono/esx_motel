00047c0cd7ac508496bda213126067820e0a3ec1
Vumono <87247208+Vumono@users.noreply.github.com>
2021-12-14 16:31:57 +0100

    Update SQL.sql

--- esx_motel/SQL.sql
+++ esx_motel/SQL.sql
@@ -1,5 +1,4 @@
 CREATE TABLE IF NOT EXISTS `motel` (
   `id` int(11) DEFAULT NULL,
   `identifier` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
-  `items` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
